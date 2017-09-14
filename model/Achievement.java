33b85df2f91b5ca3bc817041d9e048a39fa618a5
SGador <30827601+SGador@users.noreply.github.com>
2017-09-14 17:28:55 +0800

    Add files via upload

--- model/Achievement.java
+++ model/Achievement.java
@@ -1,30 +1,20 @@
 package com.ibm.achievement.dao.model;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import com.ibm.achievement.entity.AchievementDocVO;
 
 public class Achievement {
 	private int achievementId;
-	private String employeeId;
-	private String achievementTypeId;
-	private String achievementText;
-	private String approverComment;
-	private String approverId;
 	private int approverPointVal;
-	private Date endDate;
-	private Date startDate;
-	private String projectId;
 	private String statusCode;
-	private List<AchievementDocVO> achievementDoc = new ArrayList<AchievementDocVO>();
+	private String categoryId;
+	private List<AchievementDoc> achievementDoc = new ArrayList<AchievementDoc>();
 
-	public List<AchievementDocVO> getAchievementDoc() {
+	public List<AchievementDoc> getAchievementDoc() {
 		return achievementDoc;
 	}
 
-	public void setAchievementDoc(List<AchievementDocVO> achievementDoc) {
+	public void setAchievementDoc(List<AchievementDoc> achievementDoc) {
 		this.achievementDoc = achievementDoc;
 	}
 
@@ -36,46 +26,6 @@ public class Achievement {
 		this.achievementId = achievementId;
 	}
 
-	public String getEmployeeId() {
-		return employeeId;
-	}
-
-	public void setEmployeeId(String employeeId) {
-		this.employeeId = employeeId;
-	}
-
-	public String getAchievementTypeId() {
-		return achievementTypeId;
-	}
-
-	public void setAchievementTypeId(String achievementTypeId) {
-		this.achievementTypeId = achievementTypeId;
-	}
-
-	public String getAchievementText() {
-		return achievementText;
-	}
-
-	public void setAchievementText(String achievementText) {
-		this.achievementText = achievementText;
-	}
-
-	public String getApproverComment() {
-		return approverComment;
-	}
-
-	public void setApproverComment(String approverComment) {
-		this.approverComment = approverComment;
-	}
-
-	public String getApproverId() {
-		return approverId;
-	}
-
-	public void setApproverId(String approverId) {
-		this.approverId = approverId;
-	}
-
 	public int getApproverPointVal() {
 		return approverPointVal;
 	}
@@ -84,36 +34,20 @@ public class Achievement {
 		this.approverPointVal = approverPointVal;
 	}
 
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public String getProjectId() {
-		return projectId;
-	}
-
-	public void setProjectId(String projectId) {
-		this.projectId = projectId;
-	}
-
 	public String getStatusCode() {
 		return statusCode;
 	}
 
 	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;		
 	}
 
 }
