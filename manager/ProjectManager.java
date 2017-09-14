f3480f34c65d670388da364e74adf723500d3be9
thereserayza <30828902+thereserayza@users.noreply.github.com>
2017-09-14 16:42:35 +0800

    Delete ProjectManager.java

--- manager/ProjectManager.java
+++
@@ -1,8 +0,0 @@
-package com.ibm.achievement.dao.manager;
-
-public interface ProjectManager {
-	public void findAllProject();
-	public void findProjectByEmpId();
-	public void insertEmpProject();
-	public void findProjectById();
-}
