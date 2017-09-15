2b376aca41819502bbf2f7b7810843b36e08a261
thereserayza <30828902+thereserayza@users.noreply.github.com>
2017-09-15 18:27:33 +0800

    Update UserDaoImpl.java

--- impl/UserDaoImpl.java
+++ impl/UserDaoImpl.java
@@ -18,7 +18,6 @@ public class UserDaoImpl implements UserManager {
 	
 	@Override
 	public User findUserByMailId(String emailId) {
-		// TODO Auto-generated method stub
 		User user = new User();
 		String query = "SELECT * from TA_USERS where EMAIL_ADDRESS='" + emailId + "'";
 		SqlRowSet srs = jdbcTemplate.queryForRowSet(query);
@@ -34,7 +33,6 @@ public class UserDaoImpl implements UserManager {
 
 	@Override
 	public List<User> findUserByActiveFlg(String activeFlag) {
-		// TODO Auto-generated method stub
 		User user;
 		List<User> userList = new ArrayList<User>();
 		String query = "SELECT * FROM TA_USERS WHERE ACTIVE_FLAG='" + activeFlag +"'";
@@ -52,7 +50,6 @@ public class UserDaoImpl implements UserManager {
 
 	@Override
 	public List<User> findAllUser() {
-		// TODO Auto-generated method stub
 		User user;
 		List<User> userList = new ArrayList<User>();
 		String query = "SELECT * FROM TA_USERS";
@@ -70,14 +67,12 @@ public class UserDaoImpl implements UserManager {
 
 	@Override
 	public int updateActiveFlag(String emailId, String activeFlag) {
-		// TODO Auto-generated method stub
 		String query = "UPDATE TA_USERS SET ACTIVE_FLAG='" + activeFlag +"' WHERE EMAIL_ADDRESS='" + emailId + "'";
 		return jdbcTemplate.update(query);
 	}
 
 	@Override
 	public boolean insertUserData(String emailId, String password, String activeFlag, String userRole) {
-		// TODO Auto-generated method stub
 		String query = "INSERT INTO TA_USERS(EMAIL_ADDRESS,PASSWORD,ACTIVE_FLAG,USER_ROLE) VALUES('" + emailId + "','" + 
 				password + "','" + activeFlag + "','" + userRole + "')";
 		try{
@@ -92,7 +87,6 @@ public class UserDaoImpl implements UserManager {
 
 	@Override
 	public boolean deleteUserData(String emailId) {
-		// TODO Auto-generated method stub
 		String query = "DELETE FROM TA_USERS WHERE EMAIL_ADDRESS='" + emailId + "'";
 		try{
 			jdbcTemplate.update(query);
