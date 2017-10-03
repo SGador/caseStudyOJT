b9867e304dd76eb153970dc8d6f7c32ee480bcc3
thereserayza <30828902+thereserayza@users.noreply.github.com>
2017-10-03 15:39:43 +0800

    Add files via upload

--- dao.util/AchievementListeners.java
+++ dao.util/AchievementListeners.java
@@ -5,21 +5,38 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import com.ibm.achievement.dao.manager.AchievementManager;
+import com.ibm.achievement.dao.manager.EmployeeManager;
+import com.ibm.achievement.dao.manager.ProjectManager;
 import com.ibm.achievement.dao.manager.UserManager;
 
 @Component
 public class AchievementListeners{
 
 	@Autowired
+	private AchievementManager achievementManager;
+	@Autowired
+	private EmployeeManager employeeManager;
+	@Autowired
+	private ProjectManager projectManager;
+	@Autowired
 	private UserManager userManager;
 	
-	@Autowired
-	private AchievementManager achievementManager;
-	
 	@EventListener(AchievementEvent.class)
-	public AchievementManager getAchievementManger() {
+	public AchievementManager getAchievementManager() {
 		System.out.println("I was here. AM");
 		return achievementManager;
+	}
+	
+	@EventListener(EmployeeEvent.class)
+	public EmployeeManager getEmployeeManager() {
+		System.out.println("I was here. EM");
+		return employeeManager;
+	}
+	
+	@EventListener(ProjectEvent.class)
+	public ProjectManager getProjectManager() {
+		System.out.println("I was here. PM");
+		return projectManager;
 	}
 	
 	@EventListener(UserEvent.class)
